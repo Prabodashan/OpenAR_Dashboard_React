3a3097e0faadfecf8dcf062497279bb3c202bef6
Prabodashan <prabodashan14@gmail.com>
2024-08-09 20:30:25 +0530

    Added Users pages

---
+++ src/layouts/globalMissing/GlobalMissing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./globalMissing.scss";
+
+const GlobalMissing = () => {
+  return (
+    <div className="globalMissing">
+      <h1>404</h1>
+      <h3>The page you are looking for cannot be found</h3>
+    </div>
+  );
+};
+
+export default GlobalMissing;
